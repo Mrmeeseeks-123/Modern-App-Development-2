a168d3417a8e96ed243441a706a18b30036a4ede
Chinmay <dixit.chinmay123@gmail.com>
2024-03-20 20:42:33 +0530

    using fetch

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -13,19 +13,61 @@
 //     console.log("In catch",error)
 // });
 
-data={"name":"chinmay","city":"agra"};
+// data={"name":"chinmay","city":"agra"};
 
-fetch("https://httpbin.org/post",{
+// fetch("https://httpbin.org/post",{
+//     method:"POST",
+//     headers:{
+//         "Content-Type":'application/json',
+//     },
+//     body: JSON.stringify(data)
+// })
+// .then(response => response.json())
+// .then(data => {
+//     console.log('success',data);
+// })
+// .catch((error)=>{
+//     console.log("error",error);
+// });
+
+
+//for the form
+// form =new FormData(document.getElementById("my-form"));
+// fetch("https://httpbin.org/put",{
+//     method:"PUT",
+//     body:form
+// }).then(response => response.json())
+// .then(result => console.log("success",result))
+// .catch(error => console.error("error",error))
+
+// download a blob
+fetch("https://httpbin.org/image/jpeg").then((response)=>{
+    console.log("got it");
+    return response.blob();
+}).then((myblob)=>{
+    console.log(myblob);
+}).catch((error)=>{
+    console.log("error",error.message)
+})
+
+//using headers
+
+myheader= new Headers();
+myheader={
+    "Content-Type":"application/json"
+}
+
+const myrequest= new Request("https://httpbin.org/post",{
     method:"POST",
-    headers:{
-        "Content-Type":'application/json',
-    },
-    body: JSON.stringify(data)
+    headers:myheader,
+    mode:"cors",
+    cache:"default"
 })
-.then(response => response.json())
-.then(data => {
-    console.log('success',data);
-})
-.catch((error)=>{
-    console.log("error",error);
-});
+fetch(myrequest).then(response=>response.json()).then(data => console.log(data));
+
+//using async await
+async function func1(){
+response= await fetch("https://httpbin.org/uuid");
+data=await response.json();
+console.log(data);}
+func1();
