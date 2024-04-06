d9abf059abfe04178cda5b5012aefd0698b74dd3
Chinmay <dixit.chinmay123@gmail.com>
2024-04-07 00:40:46 +0530

    demonstration of using flask login

--- authentication_in_flask/app.py
+++ authentication_in_flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template 
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager,login_user,current_user,logout_user
 
 app=Flask(__name__)
 
@@ -27,5 +27,27 @@ class Article(db.model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(user_id) #TO LOAD THE SAVED USER CREDENTIALS FROM THE SESSION
+
+@app.route("/users")
+def all_users():
+    users=User.query.all()
+    return render_template("all_users.html",users=users)
+
+@app.route("/login/<name>")
+def user_login(name):
+    this_user=User.query.filter_by(username=name).first()
+    login_user(this_user) #TO SAVE USER CREDENTIALS IN THE SESSION 
+    return "user logged in:" + this_user.username
+
+
+@app.route("/all_articles")
+def all_articles():
+    articles=Article.query.filter_by(author=current_user.id).all()
+    return render_template("user_articles.html",articles=articles)
+    
+@app.route("/logout")
+def user_logout():
+    logout_user()
+    return "user logged out"
 
